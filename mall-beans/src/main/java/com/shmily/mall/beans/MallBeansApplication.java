b20563e77a1f10bd640bcf938ef279ee0cd2cd91
Yan Qingyue <854668244@qq.com>
2019-08-31 19:41:01 +0800

    Initial commit

---
+++ mall-beans/src/main/java/com/shmily/mall/beans/MallBeansApplication.java
@@ -0,0 +1,13 @@
+package com.shmily.mall.beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallBeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallBeansApplication.class, args);
+    }
+
+}
